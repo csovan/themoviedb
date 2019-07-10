eac7f8ea51e8d273427c9d45eaf42113b613bf8d
Chandara Sovan <csovan.cs@gmail.com>
2019-07-10 16:05:37 -0400

    Create movie details activity

--- app/src/main/java/com/csovan/themoviedb/ui/adapter/MovieCardLargeAdapter.java
+++ app/src/main/java/com/csovan/themoviedb/ui/adapter/MovieCardLargeAdapter.java
@@ -1,6 +1,7 @@
 package com.csovan.themoviedb.ui.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
@@ -14,10 +15,12 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.csovan.themoviedb.R;
 import com.csovan.themoviedb.data.model.movie.MovieBrief;
+import com.csovan.themoviedb.ui.activity.MovieDetailsActivity;
 
 import java.util.List;
 
 import static com.csovan.themoviedb.util.Constant.IMAGE_LOADING_BASE_URL_780;
+import static com.csovan.themoviedb.util.Constant.MOVIE_ID;
 
 public class MovieCardLargeAdapter extends RecyclerView.Adapter<MovieCardLargeAdapter.MovieViewHolder> {
 
@@ -76,6 +79,15 @@ public class MovieCardLargeAdapter extends RecyclerView.Adapter<MovieCardLargeAd
                     (int) (context.getResources().getDisplayMetrics().widthPixels * 0.85);
             cvMovieCard.getLayoutParams().height =
                     (int) ((context.getResources().getDisplayMetrics().widthPixels * 0.85) / 1.75);
+
+            cvMovieCard.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(context, MovieDetailsActivity.class);
+                    intent.putExtra(MOVIE_ID, movieList.get(getAdapterPosition()).getId());
+                    context.startActivity(intent);
+                }
+            } );
 
         }
     }
