6c4fe3d2f41669cd3c83a68a69c28ef680f2279d
Chandara Sovan <csovan.cs@gmail.com>
2019-07-10 16:29:01 -0400

    create collapsing toolbar layout

--- app/src/main/java/com/csovan/themoviedb/ui/activity/MovieDetailsActivity.java
+++ app/src/main/java/com/csovan/themoviedb/ui/activity/MovieDetailsActivity.java
@@ -2,8 +2,11 @@ package com.csovan.themoviedb.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.csovan.themoviedb.R;
+
+import java.util.Objects;
 
 public class MovieDetailsActivity extends AppCompatActivity {
 
@@ -11,5 +14,9 @@ public class MovieDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_movie_details);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
     }
 }
