b38601a917bd6b9c26ff8ed0780058749fce5bde
Chandara Sovan <csovan.cs@gmail.com>
2019-07-15 14:41:56 -0400

    Add search item layout

---
+++ app/src/main/java/com/csovan/themoviedb/ui/activity/SearchActivity.java
@@ -0,0 +1,15 @@
+package com.csovan.themoviedb.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.csovan.themoviedb.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
